0fc7ca309998fa9c9aead5713f20f8a703c45243
emanuelsistemas <emanuel.sistemas@gmail.com>
2024-12-30 18:27:00 +0000

    fix: removida dependência do sentence-transformers do setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "chromadb>=0.4.22",
+        "chromadb==0.4.22",
         "groq>=0.4.2",
         "openai>=1.12.0",
         "python-dotenv>=1.0.1",
-        "sentence-transformers>=2.2.2",
     ],
     entry_points={
         "console_scripts": [
